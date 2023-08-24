c138e8446ced603c1fef34c6db4ec33a80857dcf
Leonardo Cortes <olcortesg73@gmail.com>
2023-08-24 18:56:31 -0500

    RutasBasicasConsiguradas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/themes/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -14,7 +16,11 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 3).getTheme(),
-      home: const HomeScreen() 
+      home: const HomeScreen(),
+      routes: {
+        '/buttons': (context)=> const ButtonsScreen(),
+        '/cards':(context) => const CardsScreen(),
+      },
       // Scaffold(
       //   body: Center(
       //     child: Text('Hello World!'),
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 //import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 // import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
-
   //static const String name = 'home_screen';
 
   const HomeScreen({super.key});
@@ -41,30 +39,22 @@ class _CustomListTile extends StatelessWidget {
   });
 
   final MenuItem menuItem;
+  
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
-    
+
     return ListTile(
-      leading: Icon( menuItem.icon, color: colors.primary ),
-      trailing: Icon( Icons.arrow_forward_ios_rounded, color:colors.primary ),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
-      subtitle: Text( menuItem.subTitle ),
+      subtitle: Text(menuItem.subTitle),
       onTap: () {
-        
-        // Navigator.of(context).push(
-        //   MaterialPageRoute(
-        //     builder: (context) => const ButtonsScreen(),
-        //   ),
-        // );
-        // Navigator.pushNamed(context, menuItem.link );
+        Navigator.pushNamed(context, menuItem.link );
 
         // context.pushNamed( CardsScreen.name );
 //        context.push( menuItem.link );
-
-
       },
     );
   }
